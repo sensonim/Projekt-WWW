389c3987b4682de0cede3cc0e74cc44971481a1a
Tomasz Golonko <91921@student.pb.edu.pl>
2025-05-10 14:42:56 +0200

    Obsługa wczytywania i dodawania postów i inne

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
-
-var stan = 1;
-function Funkcja()
-{
-    var element = document.getElementById("test");
-    var tekst = document.getElementById("tekst1");
-    if (stan) {
-        element.innerHTML = "Cześć " + tekst.value;
-        element.style.color = "blue";
+async function FetchData(path = '', options = { method: 'GET' }) {
+    const url = 'http://localhost:3000' + path;
+    const promise = await fetch(url, options);
+    try {
+        return promise.json();
     }
-    else
-        element.innerHTML = "";
-    stan = !stan;
+    catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+async function ShowPosts(){
+    const posts = await FetchData('/posts?_sort=id&_order=desc');
+    const kontener = document.getElementById("posty");
+    var s = "<div class = 'feed'>";
+    posts.forEach(function (post) {
+        s = s + `
+        <div class = 'kontener_post'>
+        <h3>${post.title}</h3>
+        <p>
+        ${post.content}
+        </div>`;
+    });
+    s = s + "</div>";
+    kontener.innerHTML = s;
+}
+
+function DodajPost(title, content){
+    FetchData('/posts', { method: 'POST', 
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            title: title,
+            content: content
+        })
+     });
+}
+
+function DodajPostPrzycisk(){
+    DodajPost(document.getElementById('post_title').value, document.getElementById('post_content').value);
 }
